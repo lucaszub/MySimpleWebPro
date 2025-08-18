218e66be9f0d239181eedcdda13d29a0451b940c
lucaszub <zubiarrainlucas@gmail.com>
2025-08-18 10:35:23 -0400

    Fix broken links and add Google Analytics tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({
@@ -22,6 +23,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-V18GH4Z7V6"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-V18GH4Z7V6');
+          `}
+        </Script>
+      </head>
       <body
         className={`${inter.variable} antialiased selection:bg-emerald-300 selection:text-zinc-900 bg-zinc-50 text-zinc-900`}
       >
